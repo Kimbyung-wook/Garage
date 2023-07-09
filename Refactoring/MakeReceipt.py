b1c14db4770432fbf5c2add402f3259b037689e2
bwkim47 <lcleansky@gmail.com>
2023-07-09 15:46:07 +0900

    Seperate Calculating Phase and Formatting Phase

--- Refactoring/MakeReceipt.py
+++ Refactoring/MakeReceipt.py
@@ -4,61 +4,67 @@ import pprint
 
 def statement(invoice, plays):
 
-    def playFor(aPerformance):
-        return plays[aPerformance['playID']]
+    def renderPlainText(statementData, plays):
+        def playFor(aPerformance):
+            return plays[aPerformance['playID']]
 
-    def amountFor(aPerformance):
-        result = 0
+        def amountFor(aPerformance):
+            result = 0
 
-        if playFor(aPerformance)['type'] == "tragedy":
-            result = 40000
-            if aPerformance['audience'] > 30:
-                result += 1000 * (aPerformance['audience'] - 30)
-        elif playFor(aPerformance)['type'] == "comedy":
-            result = 30000
-            if aPerformance['audience'] > 20:
-                result += 10000 + 500 * (aPerformance['audience'] - 20)
-            result += 300 * aPerformance['audience']
-        else:
-            assert True, "Unknown genre : {:s}".format(playFor(aPerformance)['type'])
+            if playFor(aPerformance)['type'] == "tragedy":
+                result = 40000
+                if aPerformance['audience'] > 30:
+                    result += 1000 * (aPerformance['audience'] - 30)
+            elif playFor(aPerformance)['type'] == "comedy":
+                result = 30000
+                if aPerformance['audience'] > 20:
+                    result += 10000 + 500 * (aPerformance['audience'] - 20)
+                result += 300 * aPerformance['audience']
+            else:
+                assert True, "Unknown genre : {:s}".format(playFor(aPerformance)['type'])
+            return result
+
+        def volumeCreditFor(aPerformance):
+            result = 0
+            # accumulating point
+            result += max([aPerformance['audience'] - 30, 0])
+
+            # give additional point per each 5 peoples
+            if "comedy" == playFor(aPerformance)['type'] :
+                result += math.floor(aPerformance['audience'] / 5)
+            return result
+
+        def usd(aNumber):
+            return aNumber/100
+
+        def getTotalVolumeCredits():
+            result = 0
+            for perf in invoice['performances']:
+                result += volumeCreditFor(perf)
+            return result
+
+        def getTotalAmount():
+            result = 0
+            for perf in invoice['performances']:
+                result += amountFor(perf)
+            return result
+
+        result = "bills (Name : {:s})\n".format(statementData['customer'])
+
+        for perf in statementData['performances']:
+            # show bills
+            result += "\t{:15s} : $ {:7.2f} ({:3d} Seats)\n".format(playFor(perf)['name'], usd(amountFor(perf)), perf['audience']) 
+
+        result += "Total : $ {:.2f}\n".format(usd(getTotalAmount()))
+        result += "Accumulated point : {:.0f} points\n".format(getTotalVolumeCredits())
+        print(result)
         return result
-
-    def volumeCreditFor(aPerformance):
-        result = 0
-        # accumulating point
-        result += max([aPerformance['audience'] - 30, 0])
-
-        # give additional point per each 5 peoples
-        if "comedy" == playFor(aPerformance)['type'] :
-            result += math.floor(aPerformance['audience'] / 5)
-        return result
-
-    def usd(aNumber):
-        return aNumber/100
-
-    def getTotalVolumeCredits():
-        result = 0
-        for perf in invoice['performances']:
-            result += volumeCreditFor(perf)
-        return result
-
-    def getTotalAmount():
-        result = 0
-        for perf in invoice['performances']:
-            result += amountFor(perf)
-        return result
-
-    result = "bills (Name : {:s})\n".format(invoice['customer'])
-
-    for perf in invoice['performances']:
-        # show bills
-        result += "\t{:15s} : $ {:7.2f} ({:3d} Seats)\n".format(playFor(perf)['name'], usd(amountFor(perf)), perf['audience']) 
-
-    result += "Total : $ {:.2f}\n".format(usd(getTotalAmount()))
-    result += "Accumulated point : {:.0f} points\n".format(getTotalVolumeCredits())
-    print(result)
-    return result
-        
+    
+    # Main loop 
+    STATEMENTDATA = {}
+    STATEMENTDATA['customer']       = invoice['customer']
+    STATEMENTDATA['performances']   = invoice['performances']
+    return renderPlainText(STATEMENTDATA, plays)
 
 def openJSON(fileName:str):
     with open(fileName) as f:
